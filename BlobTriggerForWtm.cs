2072bdc3c5707eded6c0cb89c5c06a749d54ef89
Paul Douglas <paul.douglas@speridian.com>
2024-02-15 21:19:48 -0500

    add changes for sendgrid, but not calling yet - also get sendgrid key...

--- BlobTriggerForWtm.cs
+++ BlobTriggerForWtm.cs
@@ -2,6 +2,10 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+// isolated worker
 
 namespace Company.Function
 {
@@ -19,7 +23,39 @@ namespace Company.Function
         {
             using var blobStreamReader = new StreamReader(stream);
             var content = await blobStreamReader.ReadToEndAsync();
-            _logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n Data: {content}");
+            _logger.LogInformation($"C# Blob trigger function Processed blob1\n Name: {name} \n Data: {content}");
+     
+        }        
+    
+
+    public static async Task SendEmailWithAttachmentAsync(Stream attachmentStream, string attachmentName)
+    {
+        string sendGridApiKey = Environment.GetEnvironmentVariable("SendGridApiKey");
+        string senderEmail = Environment.GetEnvironmentVariable("SenderEmail");
+        string recipientEmail = Environment.GetEnvironmentVariable("RecipientEmail");
+
+        var client = new SendGridClient(sendGridApiKey);
+        var message = new SendGridMessage();
+        message.SetFrom(new EmailAddress(senderEmail));
+        message.AddTo(new EmailAddress(recipientEmail));
+        message.SetSubject("Blob Uploaded");
+        message.AddAttachment(attachmentName, Convert.ToBase64String(ReadFully(attachmentStream)), "application/octet-stream");
+
+        var response = await client.SendEmailAsync(message);
+        if (response.StatusCode != System.Net.HttpStatusCode.OK && response.StatusCode != System.Net.HttpStatusCode.Accepted)
+        {
+            throw new Exception($"Failed to send email. Status code: {response.StatusCode}");
         }
+    }
+
+    private static byte[] ReadFully(Stream input)
+    {
+        using (MemoryStream ms = new MemoryStream())
+        {
+            input.CopyTo(ms);
+            return ms.ToArray();
+        }
+    }
+
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Readme
+
+
+dotnet add package Microsoft.Azure.WebJobs.Extensions.Storage.Blobs --version 5.0.0
+
+dotnet add package SendGrid
